4ac947804fd921bf21d2610e3a6e79a0c62e4533
thuentenatalie <natalie.thuente@gmail.com>
2026-04-01 22:59:52 -0700

    updated routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 
@@ -7,14 +7,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="#:section" element={<Home />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="#:section" element={<Home />} />
+      </Route>
+    </Routes>
   )
 }
 
